c6233c5bd03cb8e90f9733a04c8bc33e4e2f7631
Adil Rafique <adrafiq.52@gmail.com>
2022-10-14 15:41:42 +0500

    Renames new to init for packages

--- main.go
+++ main.go
@@ -8,47 +8,48 @@ import (
 	"strings"
 )
 
-var config = utils.NewConfig()
-var logger = utils.NewLogger()
-var host = proxy.NewHost()
+func init() {
+	utils.InitConfig()
+	utils.InitLogger()
+	proxy.InitHost()
+}
 
 func main() {
 	// This code piece explictily declares ServeMux and default Server to elaborate internals
-	logger.Infof("Server is starting at %s ", config.GetString("port"))
+	utils.Logger.Infof("Server is starting at %s ", utils.Config.GetString("port"))
 	router := http.NewServeMux()
 	router.HandleFunc("/", defaultHandler)
 	server := http.Server{
-		Addr:    ":" + config.GetString("port"),
+		Addr:    ":" + utils.Config.GetString("port"),
 		Handler: router,
 	}
-	logger.Fatal(server.ListenAndServe())
+	utils.Logger.Fatal(server.ListenAndServe())
 }
 
 func defaultHandler(res http.ResponseWriter, req *http.Request) {
 
-	logger.Debugf("Request %+v", req)
-	host = proxy.HostConfigured
+	utils.Logger.Debugf("Request %+v", req)
 	hostName := strings.Split(req.Host, ":")[0]
-	if hostName != host.Name {
+	if hostName != proxy.HostConfigured.Name {
 		res.WriteHeader(403)
 		io.WriteString(res, "unrecognized host")
 		return
 	}
-	proxyTarget, _ := host.GetNext()
+	proxyTarget, _ := proxy.HostConfigured.GetNext()
 	req.URL.Host = proxyTarget
 	req.Host = "39.45.128.173" //Place holder for exteral LB
 	req.RequestURI = ""
 	client := http.DefaultClient
 	proxyRes, err := client.Do(req)
 	if err != nil {
-		logger.Error(err)
+		utils.Logger.Error(err)
 		res.WriteHeader(403)
 		io.WriteString(res, err.Error())
 		return
 	} else {
 		defer proxyRes.Body.Close()
 	}
-	logger.Debugf("Response %+v", proxyRes)
+	utils.Logger.Debugf("Response %+v", proxyRes)
 	res.WriteHeader(proxyRes.StatusCode)
 	io.Copy(res, proxyRes.Body)
 }
--- utils/logger.go
+++ utils/logger.go
@@ -8,12 +8,11 @@ import (
 
 var Logger = logrus.New()
 
-func NewLogger() logrus.Logger {
+func InitLogger() {
 	if logLevel, err := logrus.ParseLevel(string(Config.GetString("logLevel"))); err != nil {
 		panic(fmt.Errorf("bad value for logLevel. must be info or debug"))
 
 	} else {
 		Logger.SetLevel(logLevel)
 	}
-	return *Logger
 }
